98c15b7a80012902da2a336dfcc4cd1d77ceb039
Henrique Sampaio <hnrqne@gmail.com>
2023-07-26 17:05:47 -0300

    Category, H2 Database

---
+++ src/main/java/com/hnrqne/sysevent/entities/Category.java
@@ -0,0 +1,68 @@
+package com.hnrqne.sysevent.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_category")
+public class Category {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(columnDefinition = "TEXT")
+	private String description;
+	
+	public Category() {
+	}
+
+	public Category(Integer id, String description) {
+		this.id = id;
+		this.description = description;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Category other = (Category) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "Category [id=" + id + ", description=" + description + "]";
+	}
+}
