0e7fe2af65e6cce2a8b5bd3e9240ff10ba90167c
Henrique Sampaio <hnrqne@gmail.com>
2023-07-26 16:45:45 -0300

    Project start

---
+++ src/main/java/com/hnrqne/sysevent/SyseventApplication.java
@@ -0,0 +1,13 @@
+package com.hnrqne.sysevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyseventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyseventApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hnrqne/sysevent/SyseventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hnrqne.sysevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyseventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
